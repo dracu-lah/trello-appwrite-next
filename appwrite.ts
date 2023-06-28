17986414d7e994df7871716ef445c20b9a35671d
dracu-lah <nevilnicks4321@gmail.com>
2023-06-28 10:45:18 +0530

    fixed env

--- appwrite.ts
+++ appwrite.ts
@@ -4,7 +4,7 @@ const client = new Client();
 
 client
   .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECTID!);
+  .setProject(`${process.env.NEXT_PUBLIC_APPWRITE_PROJECTID!}`);
 
 const account = new Account(client);
 const databases = new Databases(client);
